e7fccb44efacf8684b6d19c9e13695858ea870a9
Tianqi Wang <wangtianqi2012@hotmail.com>
2022-01-20 22:59:29 -0800

    Initial commit

---
+++ Child_1.py
@@ -0,0 +1,21 @@
+import pygame #todo finish this file
+
+pygame.init()
+
+p = 7
+
+print (p)
+print (x)
+print (x)
+print (x)
+
+def main(): # todo finish this function
+    hello = 0
+    print(hello)
+
+run = True
+
+try:
+    run
+except:
+    pass
